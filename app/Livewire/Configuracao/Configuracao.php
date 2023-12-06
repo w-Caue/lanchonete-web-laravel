9b33fc141d99c2e63f60dbdfcc732628d8233da8
Wanderson Cauê <123313244+w-Caue@users.noreply.github.com>
2023-12-06 19:31:34 -0300

    tela de usuario

--- app/Livewire/Configuracao/Configuracao.php
+++ app/Livewire/Configuracao/Configuracao.php
@@ -3,13 +3,26 @@
 namespace App\Livewire\Configuracao;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Configuracao extends Component
 {
+    use LivewireAlert;
 
     public $showTelaUsuarios;
     public $newUser;
+
+    #[Rule('required', 'string', 'max:255')]
+    public $nomeUser;
+
+    #[Rule('required', 'string', 'email', 'max:255', 'unique:users')]
+    public $emailUser;
+
+    #[Rule('required', 'string', 'min:4', 'confirmed')]
+    public $senhaUser;
 
     public function fecharTela(){
         $this->showTelaUsuarios = false;
@@ -25,6 +38,22 @@ class Configuracao extends Component
 
     public function novoUser(){
         $this->newUser = true;
+    }
+
+    public function saveUser(){
+        User::create([
+            'name' => $this->nomeUser,
+            'email' => $this->emailUser,
+            'password' => Hash::make($this->senhaUser),
+        ]);
+
+        $this->fecharUser();
+
+        $this->alert('success', 'Usuário Cadastrado!', [
+            'position' => 'center',
+            'timer' => 1000,
+            'toast' => false,
+        ]);
     }
 
     public function render()
