11546eb6412588725df9eb64383520e1cb967c73
Caue <caue.wandersondev7@gmail.com>
2024-05-08 17:01:49 -0300

    pesquisa de produtos do ecommerce criada

--- app/Livewire/Ecommerce/Ecommerce/PesquisaProduto.php
+++ app/Livewire/Ecommerce/Ecommerce/PesquisaProduto.php
@@ -2,10 +2,45 @@
 
 namespace App\Livewire\Ecommerce\Ecommerce;
 
+use App\Models\Produto;
 use Livewire\Component;
 
 class PesquisaProduto extends Component
 {
+    public $search;
+
+    public $produtos = [];
+
+    public function mount()
+    {
+        $this->search = '';
+    }
+
+    public function updatedSearch()
+    {
+
+        if ($this->search == "") {
+            $this->produtos = [];
+            return;
+        }
+
+        $dados = Produto::select([
+            'produtos.id',
+            'produtos.nome',
+            'produtos.descricao',
+            'produtos.preco',
+            'produtos.promocao',
+            'produtos.valor_promocao',
+        ]) #Filtros
+            ->when($this->search, function ($query) {
+                return $query->where('descricao', 'LIKE', "%" . $this->search . "%");
+            })
+            ->get()->take(5);
+
+       $this->produtos = $dados;
+
+    }
+
     public function render()
     {
         return view('livewire.ecommerce.ecommerce.pesquisa-produto');
